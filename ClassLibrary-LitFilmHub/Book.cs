bd06a7c4587111cbd9eb28086b958ce7da3f047c
Jonathan <cordovaj63@gmail.com>
2023-11-15 19:15:46 -0800

    installed Microsoft.EntityFrameworkCore.Tools, Microsoft.EntityFrameworkCore.SqlServer, Microsoft.EntityFrameworkCore.Design, version 7.0.11

---
+++ ClassLibrary-LitFilmHub/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary_LitFilmHub;
+
+public partial class Book
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Author { get; set; }
+
+    public string? Genre { get; set; }
+
+    public int? PublicationYear { get; set; }
+
+    public string? Isbn { get; set; }
+
+    public string? Summary { get; set; }
+
+    public string? CoverImageUrl { get; set; }
+}
---
+++ ClassLibrary-LitFilmHub/Film.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary_LitFilmHub;
+
+public partial class Film
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Director { get; set; }
+
+    public string? Genre { get; set; }
+
+    public int? ReleaseYear { get; set; }
+
+    public string? Rating { get; set; }
+
+    public string? Synopsis { get; set; }
+
+    public string? PosterImageUrl { get; set; }
+}
