a6698707a2dd6d1b29065ba7c8c4e86d1619fd2d
Jonathan <cordovaj63@gmail.com>
2023-11-17 10:44:15 -0800

    Configuring the Class Library to work with database.

--- ClassLibrary-LitFilmHub/LiteratureAndFilmDbContext.cs
+++ ClassLibrary-LitFilmHub/LiteratureAndFilmDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ClassLibrary_LitFilmHub;
 
@@ -19,9 +20,20 @@ public partial class LiteratureAndFilmDbContext : DbContext
 
     public virtual DbSet<Film> Films { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=LiteratureAndFilmDB;Integrated Security=true");
+       // => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=LiteratureAndFilmDB;Integrated Security=true");
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        IConfiguration configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
+
+
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
