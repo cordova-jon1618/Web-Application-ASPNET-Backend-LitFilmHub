9e2e1a8de31a34a7978f522779ad8fef646e29c9
Jonathan <cordovaj63@gmail.com>
2023-11-17 10:25:31 -0800

    Created Migrations initial configuration in Class Library

---
+++ ClassLibrary-LitFilmHub/Migrations/20231117182133_Initial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassLibrary_LitFilmHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    PublicationYear = table.Column<int>(type: "int", nullable: true),
+                    ISBN = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CoverImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Director = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: true),
+                    Rating = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Synopsis = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PosterImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
