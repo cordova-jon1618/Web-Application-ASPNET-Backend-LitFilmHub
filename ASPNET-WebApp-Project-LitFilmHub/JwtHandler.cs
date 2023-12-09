c142e0dae0ccea1b6f5f33eb1b501a738f2e1c7a
Jonathan <cordovaj63@gmail.com>
2023-12-08 18:00:46 -0800

    Updated Program.cs, JwtHandler.cs, and added 7.0.12 version dependencies for AspNetCore.Authentication.JwtBearer and ASPNetCore.Identity.EntityFrameworkCore

---
+++ ASPNET-WebApp-Project-LitFilmHub/JwtHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ClassLibrary_LitFilmHub;
+
+namespace ASPNET_WebApp_Project_LitFilmHub
+{
+    public class JwtHandler
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<LiteratureAndFilmUser> _userManager;
+        public JwtHandler(IConfiguration configuration, UserManager<LiteratureAndFilmUser> userManager)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+
+        public async Task<JwtSecurityToken> GetTokenAsync(LiteratureAndFilmUser user) =>
+            new(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: await GetClaimsAsync(user),
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_configuration["JwtSettings:ExpirationTimeInMinutes"])),
+                signingCredentials: GetSigningCredentials());
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            byte[] key = Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecurityKey"]!);
+            SymmetricSecurityKey secret = new(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaimsAsync(LiteratureAndFilmUser user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Name, user.UserName!)
+            };
+            claims.AddRange(from role in await _userManager.GetRolesAsync(user) select new Claim(ClaimTypes.Role, role));
+            return claims;
+        }
+    }
+}
--- ASPNET-WebApp-Project-LitFilmHub/Program.cs
+++ ASPNET-WebApp-Project-LitFilmHub/Program.cs
@@ -1,5 +1,15 @@
-using Microsoft.EntityFrameworkCore;
+using ASPNET_WebApp_Project_LitFilmHub;
 using ClassLibrary_LitFilmHub;
+using ClassLibrary_LitFilmHub.Migrations;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +19,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<LiteratureAndFilmDbContext>(optionsAction: OptionsBuilder => 
+builder.Services.AddDbContext<LiteratureAndFilmDbContext>(optionsAction: OptionsBuilder =>
     OptionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+builder.Services.AddIdentity<LiteratureAndFilmUser, IdentityRole>()
+        .AddEntityFrameworkStores<LiteratureAndFilmDbContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}
+).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+
+        ValidIssuer = builder.Configuration["JwtSettings.Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings.Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+            builder.Configuration["JwtSettings:SecurityKey"] ?? throw new InvalidOperationException()))
+    };
+}
+);
+
+builder.Services.AddScoped<JwtHandler>();
+//var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,6 +61,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
